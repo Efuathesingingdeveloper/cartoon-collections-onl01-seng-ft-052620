53613e4476780fe4bd14e3168e9b13d1a23ec510
Georgette Class-Peters <georgycp@udel.edu>
2020-06-05 18:44:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def summon_captain_planet(veggies)# code an argument here
 end
 
 def long_planteer_calls(long_planteer_calls)# code an argument here
-  # Your code here
+# Your code here
   answer = false
   long_planteer_calls.each do |call|
     if call.length > 4
